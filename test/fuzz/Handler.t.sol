869041d5a9cd0c3db54d4a4d71cded6f7a9b6d55
gulunn <mcliu592123@gmail.com>
2024-08-22 11:48:55 +0800

    fuzz test

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -1,2 +1,83 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DscEngine} from "../../src/DscEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+
+contract Handler is Test {
+    DecentralizedStableCoin dsc;
+    DscEngine dscEngine;
+    ERC20Mock weth;
+    ERC20Mock wbtc;
+
+    address[] userDeposited;
+
+    uint256 MAX_DEPOSIT_SIZE = type(uint96).max;
+
+    constructor(DecentralizedStableCoin _dsc, DscEngine _dscEngine) {
+        dsc = _dsc;
+        dscEngine = _dscEngine;
+
+        address[] memory collateralTokens = dscEngine.getCollateralTokens();
+        weth = ERC20Mock(collateralTokens[0]);
+        wbtc = ERC20Mock(collateralTokens[1]);
+    }
+
+    function depositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        amountCollateral = bound(amountCollateral, 1, MAX_DEPOSIT_SIZE);
+
+        vm.startPrank(address(msg.sender));
+        collateral.mint(address(msg.sender), amountCollateral);
+        collateral.approve(address(dscEngine), amountCollateral);
+        dscEngine.depositCollateral(address(collateral), amountCollateral);
+        vm.stopPrank();
+
+        // Record user deposited
+        userDeposited.push(msg.sender);
+    }
+
+    function redeemCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        uint256 collateralBalance = dscEngine.getCollateralBalanceOfUser(msg.sender, address(collateral));
+        console.log("collateralBalance", collateralBalance);
+        // delete this
+        // TODO: redeemCollateral only checks if user has enough collateral, but not if they have minted dsc
+        // so that they could break health factor if they redeem all collateral
+        (uint256 dscMinted, uint256 collateralValue) = dscEngine.getAccountInformation(msg.sender);
+        console.log("dscMinted", dscMinted);
+        console.log("collateralValue", collateralValue);
+
+        amountCollateral = bound(amountCollateral, 0, collateralBalance);
+        // If the amount is 0, we skip this fuzz run
+        if (amountCollateral == 0) return;
+        vm.prank(msg.sender);
+        dscEngine.redeemCollateral(address(collateral), amountCollateral);
+    }
+
+    // function mintDsc(uint256 _amount, uint256 _addressSeed) public {
+    //     if (userDeposited.length == 0) return;
+    //     console.log("user array length", userDeposited.length);
+    //     address userAddress = userDeposited[_addressSeed % userDeposited.length];
+    //     (uint256 dscMinted, uint256 collateralValue) = dscEngine.getAccountInformation(userAddress);
+    //     uint256 maxDscToMint = (collateralValue / 2) - dscMinted;
+    //     if (maxDscToMint <= 0) return;
+    //     _amount = bound(_amount, 0, maxDscToMint);
+    //     if (_amount == 0) return;
+    //     vm.prank(userAddress);
+    //     dscEngine.mintDsc(_amount);
+    // }
+
+    /////////////////////////////////////////////////////////
+    //                  Helper functions                   //
+    /////////////////////////////////////////////////////////
+    function _getCollateralFromSeed(uint256 seed) internal view returns (ERC20Mock) {
+        if (seed % 2 == 0) {
+            return weth;
+        } else {
+            return wbtc;
+        }
+    }
+}
--- test/fuzz/InvariantsTest.t.sol
+++ test/fuzz/InvariantsTest.t.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
 import {DeployDsc} from "../../script/DeployDsc.s.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {DscEngine} from "../../src/DscEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Handler} from "./Handler.t.sol";
 
 contract InvariantsTest is StdInvariant, Test {
     DeployDsc deployer;
@@ -14,8 +16,35 @@ contract InvariantsTest is StdInvariant, Test {
     DscEngine dscEngine;
     DecentralizedStableCoin dsc;
 
+    address wethAddress;
+    address wbtcAddress;
+
     function setUp() external {
         deployer = new DeployDsc();
         (dsc, dscEngine, helperConfig) = deployer.run();
+        (wethAddress, wbtcAddress,,,) = helperConfig.activeNetworkConfig();
+        Handler handler = new Handler(dsc, dscEngine);
+        // Set the target contract for the invariant checks
+        targetContract(address(handler));
+    }
+
+    function invariant_protocolMustHaveMoreValueThanTotalSupply() external view {
+        uint256 totalSupply = dsc.totalSupply();
+
+        uint256 totalWethDeposited = IERC20(wethAddress).balanceOf(address(dscEngine));
+        uint256 totalWbtcDeposited = IERC20(wbtcAddress).balanceOf(address(dscEngine));
+        uint256 totalWethValue = dscEngine.getUsdValue(wethAddress, totalWethDeposited);
+        uint256 totalWbtcValue = dscEngine.getUsdValue(wbtcAddress, totalWbtcDeposited);
+
+        console.log("weth value:", totalWethValue);
+        console.log("wbtc value:", totalWbtcValue);
+        console.log("total supply:", totalSupply);
+
+        assert(totalSupply <= totalWethValue + totalWbtcValue);
+    }
+
+    function invariant_gettersShouldNotRevert() public view {
+        dscEngine.getCollateralTokens();
+        dscEngine.getMinHealthFactor();
     }
 }
